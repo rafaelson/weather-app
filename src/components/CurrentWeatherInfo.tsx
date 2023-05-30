7659c4fb75323de44079026ba5ecdb739b9a7bd0
Rafael Nelson <rafaelson1337@gmail.com>
2023-05-29 22:35:55 -0300

    current weather page is fully functional

--- src/components/CurrentWeatherInfo.tsx
+++ src/components/CurrentWeatherInfo.tsx
@@ -1,6 +1,19 @@
 import CurrentWeatherInfoProps from "../interfaces/CurrentWeatherInfoProps";
 
 export default function CurrentWeatherInfo(props: CurrentWeatherInfoProps) {
+  const kelvinToCelsius = (kelvinTemp: number) =>
+    Math.round(kelvinTemp - 273.15);
+  const msToKmh = (ms: number) => Math.round(ms * 3.6);
+  const checkIfNight = () => {
+    const sunriseUnix = props.weatherData.current?.sys?.sunrise!;
+    const sunsetUnix = props.weatherData.current?.sys?.sunset!;
+    const currentUnixTime = Math.floor(new Date().getTime() / 1000);
+
+    if (currentUnixTime < sunsetUnix && currentUnixTime > sunriseUnix) {
+      return "day";
+    } else return "night";
+  };
+
   return (
     <div
       css={{
@@ -17,26 +30,29 @@ export default function CurrentWeatherInfo(props: CurrentWeatherInfoProps) {
           marginBottom: "15px",
         }}
       >
-        Weather in Resende
+        Weather in {props.weatherData.current?.name}
       </span>
       <span css={{ fontWeight: 800, fontSize: "50px", lineHeight: "35px" }}>
-        30ºC
+        {kelvinToCelsius(props.weatherData.current?.main?.temp!)}ºC
       </span>
       <span css={{ fontWeight: 600, fontSize: "30px", lineHeight: "45px" }}>
-        feels like 32ºC
+        feels like{" "}
+        {kelvinToCelsius(props.weatherData.current!.main?.feelsLike!)}ºC
       </span>
       <span css={{ fontWeight: 500, fontSize: "20px", lineHeight: "30px" }}>
         <span
           css={{ fontSize: "30px", marginRight: "10px" }}
-          className="wi wi-owm-night-802"
+          className={`wi wi-owm-${checkIfNight()}-${
+            props.weatherData.current?.weather![0].id
+          }`}
         ></span>
-        Cloudy
+        {props.weatherData.current?.weather![0].main}
       </span>
       <span css={{ fontWeight: 500, fontSize: "20px", lineHeight: "30px" }}>
-        Humidity: 45%
+        Humidity: {props.weatherData.current?.main?.humidity}%
       </span>
       <span css={{ fontWeight: 500, fontSize: "20px", lineHeight: "30px" }}>
-        Wind Speed: 3.4 km/h
+        Wind Speed: {msToKmh(props.weatherData.current?.wind?.speed!)} km/h
       </span>
     </div>
   );
